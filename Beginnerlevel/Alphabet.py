edd2539c1f44864e0a1c70c625ee6402c92d5c99
vbkavin07 <37337085+vbkavin07@users.noreply.github.com>
2018-03-20 19:48:53 +0530

    Create Alphabet.py

---
+++ Beginnerlevel/Alphabet.py
@@ -0,0 +1,8 @@
+ch = input("Enter any character: ");
+if ch == '0':
+    exit();
+else:
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    	print(ch, "is an alphabet.");
+    else:
+    	print(ch, "is not an alphabet.")
