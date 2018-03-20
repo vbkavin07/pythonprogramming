21419daa0f3e25219700ad6903506e15c54e0a6f
vbkavin07 <37337085+vbkavin07@users.noreply.github.com>
2018-03-20 10:44:13 +0530

    Update vowel.py

--- beginnerlevel/vowel.py
+++ beginnerlevel/vowel.py
@@ -1,6 +1,5 @@
-_1=str(input())
-print("enter a character")
-if _1 in('a'or'A','e'or'E','i'or'I','o'or'O','u'or'U'):
+char=str(input())
+if char in('a'or'A','e'or'E','i'or'I','o'or'O','u'or'U'):
     print("vowel")
 else:
     print("consonant")
