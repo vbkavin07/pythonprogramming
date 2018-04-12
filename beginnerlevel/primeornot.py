8e10a1ce4d9235801c74d3ea833a502bd8aeb39c
vbkavin07 <37337085+vbkavin07@users.noreply.github.com>
2018-04-12 18:13:43 +0530

    Create primeornot.py

---
+++ beginnerlevel/primeornot.py
@@ -0,0 +1,12 @@
+a=int(input(""))
+if(a<1000):
+	k=0
+	for i in range(2,a//2+1):
+	    if(a%i==0):
+	        k=k+1
+	if(k<=0):
+	    print("Number is prime")
+	else:
+	    print("Number isn't prime")
+else:
+	print("no")
